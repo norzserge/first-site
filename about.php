c359fc6847796d090295e72d85b062b4fd9aef9a
Sergey Metelskiy <metelskiisa@mail.ru>
2015-01-23 12:36:38 +0300

    Вывод мета-тегов через require

--- about.php
+++ about.php
@@ -1,15 +1,7 @@
 ﻿<!DOCTYPE html>
 <html>
 <head>
-	<meta http-equiv="content-type" content="text/html; charset=utf-8" />
-	<link rel="shortcut icon" href="favicon.ico" type="image/x-icon">
-	<link rel="icon" href="favicon.ico" type="image/x-icon">
-	<link rel="stylesheet" type="text/css" href="css/normalize.css" />
-	<link rel="stylesheet/less" type="text/css" href="css/style.less" />
-	<link rel="stylesheet" type="text/css" href="js/soc.js/soc.min.css" />
-	<script type="text/javascript" src="js/less.min.js"></script>
-	<script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
-	<script type="text/javascript" src="js/myScripts.js"></script>
+	<?php require_once("pages/head.php"); ?>
 	<title>Обо мне | LoftSchool</title>
 </head>
 <body>
--- portfolio.php
+++ portfolio.php
@@ -1,18 +1,8 @@
 ﻿<!DOCTYPE html>
 <html>
 <head>
-	<meta http-equiv="content-type" content="text/html; charset=utf-8" />
-	<link rel="shortcut icon" href="favicon.ico" type="image/x-icon">
-	<link rel="icon" href="favicon.ico" type="image/x-icon">
-	<link rel="stylesheet" type="text/css" href="css/normalize.css" />
-	<link rel="stylesheet/less" type="text/css" href="css/style.less" />
-	<link rel="stylesheet" type="text/css" href="js/soc.js/soc.min.css" />
-	<script type="text/javascript" src="js/less.min.js"></script>
-	<script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
-	<script type="text/javascript" src="js/myScripts.js"></script>
-	<script type="text/javascript" src="js/modal.js"></script>
-	<meta name="viewport" content="width=device-width,initial-scale=1">
-	<title>Портфолио | LoftSchool</title>
+	<?php require_once("pages/head.php"); ?>
+	<title>Портфолио | LoftSchool</title>	
 </head>
 <body>
 	<header>							<!-- HEADER -->
